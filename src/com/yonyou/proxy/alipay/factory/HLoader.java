ae0f926383d19320d5ba50df414270871b0f4a99
chizk <chizengkun@163.com>
2017-07-01 09:52:34 +0800

    commit 增加控制

---
+++ src/com/yonyou/proxy/alipay/factory/HLoader.java
@@ -0,0 +1,20 @@
+package com.yonyou.proxy.alipay.factory;
+
+import javax.servlet.ServletContext;
+
+import com.ufida.hap.context.loader.ext.SystemLoader;
+import com.ufida.hap.core.exception.FrameworkException;
+
+public class HLoader implements SystemLoader {
+
+	public void destroy() throws FrameworkException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init(ServletContext arg0) throws FrameworkException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
